02f5162cc79940237434b4ea01055e8ffb671ea1
Nicolas Bauwens <nicolas.bauwens@gmail.com>
2017-10-11 15:32:37 +0200

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var meals = [
+  {
+    "name":"Super cheesy burger",
+    "description":"Discover our great cheesy burger with mozzarella, gorgonzola and pecorino",
+    "price":8.5,
+    "veggie":true,
+    "quantity":0
+  },
+  {
+    "name":"Bad beef burger",
+    "description":"Full of angry beef, jalapeno peppers, fried onions and our special BBQ sauce",
+    "price":12,
+    "veggie":false,"quantity":0
+  },
+  {
+    "name":"Spicy chili burger",
+    "description":"HOT HOT HOT HOT HOT HOT",
+    "price":10,
+    "veggie":false,
+    "quantity":0
+  }
+];
